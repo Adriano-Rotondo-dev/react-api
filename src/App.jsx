f8b2fa91ea2ae80089bc95a0a6d24d823f6468f2
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-07-22 18:36:05 +0200

    Add search input to header component, refactor header component buttons and search bar

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
   const [actresses, setActresses] = useState([]);
   const [actors, setActors] = useState([]);
   const [render, setRender] = useState("both");
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     //* axios request per ottenere i dati delle attrici
@@ -25,20 +26,28 @@ function App() {
       .catch((err) => console.error(err));
   }, []);
 
+  const filterActresses = actresses.filter((a) =>
+    a.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  const filterActors = actors.filter((a) =>
+    a.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <>
-      <Header setRender={setRender} />
+      <Header setRender={setRender} search={search} setSearch={setSearch} />
       <div className="container flex">
         <div className="row flex">
           {(render == "actresses" || render === "both") && (
             <div className="row flex">
-              <ActressesMap actresses={actresses} />
+              <ActressesMap actresses={filterActresses} />
             </div>
           )}
         </div>
         <div className="row flex">
           {(render == "actors" || render === "both") && (
-            <ActorsMap actors={actors} />
+            <ActorsMap actors={filterActors} />
           )}
         </div>
       </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,31 @@
 import Btn from "./btn/Btn";
 
-export default function Header({ setRender }) {
+export default function Header({ setRender, search, setSearch }) {
   return (
     <header className="flex">
       <h1>Famous Actresses and Actors</h1>
-      <Btn
-        label="Show Only Actresses"
-        value="actresses"
-        setRender={setRender}
-      />
-      <Btn label="Show Only Actors" value="actors" setRender={setRender} />
-      <Btn
-        label="Show Both Actors and Actresses"
-        value="both"
-        setRender={setRender}
-      />
+      <div className="selectContainer flex">
+        <div className="btnContainer flex">
+          <Btn
+            label="Show Only Actresses"
+            value="actresses"
+            setRender={setRender}
+          />
+          <Btn label="Show Only Actors" value="actors" setRender={setRender} />
+          <Btn
+            label="Show Both Actors and Actresses"
+            value="both"
+            setRender={setRender}
+          />
+        </div>
+        <input
+          className="input"
+          type="text"
+          placeholder="Search by Name Here..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
     </header>
   );
 }
